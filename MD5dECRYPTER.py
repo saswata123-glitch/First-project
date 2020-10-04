21ecf5b88e53bfffee07719c8dec4b124a1e1ed2
saswata123-glitch <72347080+saswata123-glitch@users.noreply.github.com>
2020-10-04 20:32:52 +0530

    Update MD5dECRYPTER.py

--- MD5dECRYPTER.py
+++ MD5dECRYPTER.py
@@ -1,9 +1,9 @@
 import hashlib
-n = input("Enter your hash");
-with open ("file text" , "r") as f;
-for lines in f;
-hash = hashlib.md5(lines).hexdigest()
-if hash = = n;
-print("[+]hash has been decoded; lines)
-break
-f.close()
+n = input("Enter your hash")
+with open ("file text" , "r") as f:
+    for lines in f:
+       hash = hashlib.md5(lines).hexdigest()
+       if hash = = n:
+        print("[+]hash has been decoded: lines)
+        break
+   f.close()
