f509f4e8f3ad29c4963c18161aee60a9f6aafce6
saswata123-glitch <72347080+saswata123-glitch@users.noreply.github.com>
2020-10-05 12:53:23 +0530

    Update MD5dECRYPTER.py

--- MD5dECRYPTER.py
+++ MD5dECRYPTER.py
@@ -4,6 +4,6 @@ with open ("file text" , "r") as f:
     for lines in f:
        hash = hashlib.md5(lines).hexdigest()
        if hash == n:
-         print("[+]hash has been decoded: lines)
+         print("[+]hash has been decoded :", lines)
          break
    f.close()
