01d29ef3e7d22d5190ae9efcc9efa022fe6170ee
saswata123-glitch <72347080+saswata123-glitch@users.noreply.github.com>
2020-10-06 10:46:17 +0530

    Update MD5dECRYPTER.py

--- MD5dECRYPTER.py
+++ MD5dECRYPTER.py
@@ -1,7 +1,7 @@
 import hashlib
 n = input("Enter your hash")
 hash_n=hashlib.md5(n.encode('UTF-8').hexdigest()
-with open ("file.txt" , "r") as f:
+with open("file.txt","r") as f:
     for lines in f:
        hash = hashlib.md5(lines.encode('UTF-8')).hexdigest()
        if hash_n == hash:
