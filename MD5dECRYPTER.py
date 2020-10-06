bcab060255640c257971eeebb6e7a19bde17f907
saswata123-glitch <72347080+saswata123-glitch@users.noreply.github.com>
2020-10-06 10:41:58 +0530

    Update MD5dECRYPTER.py

--- MD5dECRYPTER.py
+++ MD5dECRYPTER.py
@@ -1,9 +1,10 @@
 import hashlib
 n = input("Enter your hash")
+hash_n=hashlib.md5(n.encode('UTF-8').hexdigest()
 with open ("file.txt" , "r") as f:
     for lines in f:
        hash = hashlib.md5(lines.encode('UTF-8')).hexdigest()
-       if hash == n:
+       if hash_n == hash:
          print("[+]hash has been decoded :", lines)
          break
   
